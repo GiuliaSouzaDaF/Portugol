3ed0a2968055d77cc321564a37d414918ecfe5e6
GiuliaSouzaDaF <giuliasouzadafonsecasc@gmail.com>
2024-10-01 22:08:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,17 @@
-function executar1008(){
-    let id, horas, valorHora, salario 
-    // let id
-    // let valorHora
-    // let salario
-    id = Number(prompt("digite seu id: "));
-    // id = Number('45');
-    // id = 45
-    horas = Number(prompt("quantas horas voce trabalhou?"));
-    valorHora = Number(prompt("quanto voce ganha por hora?"));
-    
-    salario = horas * valorHora 
-    alert("ID: "+ id + "\nsalario: R$"+salario.toFixed(2));
+let batatas = [];
+function add(){
+    let produto = document.getElementById("txt").value;
+    batatas.push(produto);
+    document.getElementById("lista").innerHTML = batatas;
 
 }
-function executar1009(){
-    // comissao = vendas * 15/100 
-    let nome, salarioFixo,totalVendas, comissao, totalReceber
-    nome = (prompt("digite o nome do vendedor"));
-    salarioFixo = Number(prompt("digite seu salario fixo: "));
-    totalVendas = Number(prompt("digite o total de vendas no mes"));
-    comissao = totalVendas*15/100
-    totalReceber = comissao + salarioFixo
-
-
-    alert("nome: "+nome+"\ntotalReceber: R$"+totalReceber.toFixed(2));
-}
-function executar1038(){
-    let codigo,qtd, total
-    codigo = Number(prompt("digite 1 para cachorro quente: \ndigite 2 para x-salada: \ndigite 3 para x-bacon: \ndigite 4 para torrada simples: \ndigite 5 para refrigerante: \n "));
-    total
-    qtd = Number(prompt("digite a quantidade de itens:"));
-    if(qtd == 1){
-        alert("total: R$" )
+function remove(){
+    let produto = document.getElementById("txt").value;
+    let posicao = batatas.indexOf(produto);
+    if(posicao == -1){
+    alert("o nome que você digitou não foi encontrado.")}
+    else{
+    batatas.splice(posicao, 1);
+    document.getElementById("lista").innerHTML = batatas;
     }
-    
 }
